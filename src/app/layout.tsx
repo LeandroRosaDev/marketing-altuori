66a105e52fd9d984484bdbf4b6ebe98dfb3c5b34
LeandroRosaDev <LeandroRosaDev>
2024-06-27 15:08:48 -0300

    Montando a estrutura inicial do site

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Marketing Altuori",
+  description:
+    "Em busca de uma agência séria para trazer o público certo para o seu negócio? Parabéns você finalmente encontrou!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,377 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <main>
+      <header className="bg-[url('/assets/bg/1.png')] bg-cover bg-center h-[1000px]">
+        <div>
+          <img src="/assets/logotipo.png" alt="" />
+          <nav>
+            <ul>
+              <li>
+                <Link href="">INÍCIO</Link>
+              </li>
+              <li>
+                <Link href="">SOBRE NÓS</Link>
+              </li>
+              <li>
+                <Link href="">SERVIÇOS</Link>
+              </li>
+              <li>
+                <Link href="">CONTATO</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <h1>
+            O Futuro do Marketing Digital <span>está Aqui.</span>
+          </h1>
+          <Link href="">CONFIRA NOSSOS PLANOS</Link>
+        </div>
+      </header>
+      <section id="sobre" className="bg-[#DDD8D3]">
+        <div>
+          <div>
+            <p>Sobre Nós</p>
+            <h1>Entenda porque somos apaixonados pelo nosso trabalho</h1>
+          </div>
+          <div>
+            <div>
+              <div>
+                <p>O início</p>
+                <p>
+                  Desde a nossa fundação em 2019, a Marketing Altuori tem sido
+                  uma força pioneira no universo do marketing digital. Com uma
+                  trajetória marcada por inovação, resultados excepcionais e um
+                  compromisso inabalável com o sucesso de nossos clientes,
+                  estabelecemo-nos rapidamente como líderes neste espaço
+                  competitivo. O que nos diferencia não é apenas o que fazemos,
+                  mas como fazemos e por que fazemos.
+                </p>
+              </div>
+              <div>
+                <p>Resultados que Falam por Si</p>
+                <p>
+                  A excelência não é apenas um objetivo; é o nosso padrão. Na
+                  Marketing Altuori, entregamos mais do que promessas –
+                  entregamos resultados concretos. Através de estratégias
+                  personalizadas, baseadas em dados e orientadas para o
+                  desempenho, impulsionamos a visibilidade, o engajamento e o
+                  crescimento de negócios em uma variedade de setores. Nossa
+                  abordagem é simples: entender profundamente as metas de nossos
+                  clientes e então superá-las, uma campanha de sucesso por vez.
+                </p>
+              </div>
+              <div>
+                <p>Uma Abordagem Distinta</p>
+                <p>
+                  Diferenciamo-nos de outras agências pelo nosso compromisso com
+                  a inovação e o aprendizado contínuo. A paisagem do marketing
+                  digital está sempre evoluindo, e a Marketing Altuori está
+                  sempre à frente da curva, antecipando tendências e adaptando
+                  estratégias para garantir o máximo impacto. Nosso segredo? Uma
+                  combinação imbatível de tecnologia de ponta, criatividade sem
+                  limites e uma análise de dados rigorosa.
+                </p>
+              </div>
+              <div>
+                <p>Uma Equipe Dedicada</p>
+                <p>
+                  O coração da Marketing Altuori é nossa equipe de profissionais
+                  dedicados. Cada membro traz consigo uma riqueza de
+                  conhecimento, experiência e, acima de tudo, uma paixão genuína
+                  pelo marketing digital. Juntos, formamos uma força coesa,
+                  comprometida em oferecer não apenas resultados, mas também uma
+                  experiência excepcional para cada cliente com quem
+                  trabalhamos.
+                </p>
+              </div>
+              <div>
+                <p>Abrace o futuro conosco</p>
+                <p>
+                  Na Marketing Altuori, estamos sempre olhando para o futuro,
+                  prontos para abraçar novos desafios e continuar a redefinir o
+                  que é possível no marketing digital. Se você está procurando
+                  uma agência que combina estratégia comprovada, inovação
+                  contínua e um compromisso genuíno com o seu sucesso, sua busca
+                  termina aqui. Bem-vindo ao próximo capítulo do seu sucesso.
+                  Bem-vindo à Marketing Altuori.
+                </p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src="/assets/icones/1.svg" alt="" />
+                <div>
+                  <p>Inovação Constante</p>
+                  <p>
+                    Nossa paixão é alimentada pela constante busca por inovação.
+                    Estamos sempre à frente, explorando novas tendências,
+                    tecnologias e estratégias para manter nossos clientes um
+                    passo à frente da concorrência. Acreditamos que a
+                    criatividade combinada com a inovação é a chave para
+                    desbloquear o verdadeiro potencial do Marketing Digital.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <img src="/assets/icones/2.svg" alt="" />
+                <div>
+                  <p>Resultados Extraordinários</p>
+                  <p>
+                    Somos movidos pelos resultados. Nossa paixão se traduz em
+                    campanhas de marketing digital sob medida, que não apenas
+                    atingem, mas superam os objetivos de nossos clientes.
+                    Combinamos análise de dados com criatividade para garantir
+                    que cada ação tenha um impacto significativo.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <img src="/assets/icones/3.svg" alt="" />
+                <div>
+                  <p>Relacionamentos Duradouros</p>
+                  <p>
+                    Acreditamos na construção de relacionamentos duradouros,
+                    tanto com nossos clientes quanto com nosso público. Para
+                    nós, o marketing digital vai além das vendas; é sobre criar
+                    conexões autênticas e significativas. Essa paixão por criar
+                    laços verdadeiros é o que nos impulsiona a buscar a
+                    excelência todos os dias.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <img src="/assets/icones/4.svg" alt="" />
+                <div>
+                  <p>Aprendizado Contínuo</p>
+                  <p>
+                    Desde a nossa fundação em 2019, a Marketing Altuori tem sido
+                    uma força pioneira no universo do marketing digital. Com uma
+                    trajetória marcada por inovação, resultados excepcionais e
+                    um compromisso inabalável com o sucesso de nossos clientes,
+                    estabelecemo-nos rapidamente como líderes neste espaço
+                    competitivo. O que nos diferencia não é apenas o que
+                    fazemos, mas como fazemos e por que fazemos.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="servicos">
+        <div>
+          <h1>
+            Especialistas em Fazer sua Empresa <span>Aparecer</span>
+          </h1>
+          <div>
+            <div>
+              <img src="/assets/img/1.png" alt="" />
+              <p>
+                Planejamento de Mídias Sociais, com integração com Ecossistema
+                Meta
+              </p>
+            </div>
+            <div>
+              <img src="/assets/img/2.png" alt="" />
+              <p>Criação de Logo, Todas as Artes para Mídias Sociais e Sites</p>
+            </div>
+            <div>
+              <img src="/assets/img/3.png" alt="" />
+              <p>
+                Construção de Sites Atrativos, com domínios próprios e
+                Hospedagem inclusas
+              </p>
+            </div>
+            <div>
+              <img src="/assets/img/4.png" alt="" />
+              <p>
+                Gestão de Tráfego Pago com criação de Campanhas que convertem
+              </p>
+            </div>
+            <div>
+              <img src="/assets/img/5.png" alt="" />
+              <p>
+                Equipe especializada em engajamento e crescimento de novas
+                marcas
+              </p>
+            </div>
+            <div>
+              <img src="/assets/img/6.png" alt="" />
+              <p>Automação de Processo de Vendas e Atendimento</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="planos" className="bg-[#474747]">
+        <div>
+          <div>
+            <h1>
+              Pare de perder tempo com Agências que não entregam{" "}
+              <span>resultados.</span>
+            </h1>
+            <img
+              src="/assets/bg/2.png"
+              alt="Imagem de uma agência prosperando"
+            />
+            <h1>
+              Confiram nossos planos e se surpreendam com nossos{" "}
+              <span>Valores.</span>.
+            </h1>
+          </div>
+          <div>
+            <div>
+              <div>
+                <h1>Plano Iniciante</h1>
+                <p>
+                  Criação de Artes, Logotipos, Redes Sociais e Integração com o
+                  Ecossistema Meta. Consultoria profissional de tráfego pago e
+                  prospeção da marca no google.
+                </p>
+              </div>
+              <div>
+                <p>R$ 799,99</p>
+                <p>R$ 299,99</p>
+              </div>
+              <a href="">Clique e Saiba Mais</a>
+            </div>
+            <div>
+              <div>
+                <h1>Plano Intermediário</h1>
+                <p>
+                  Todas as opções do nosso Plano Iniciante + Criação e
+                  Hospedagem de Site estilo landing Page + Domínio Grátis com
+                  todas as otimizações para marketing Digital + Tráfego Pago
+                  personalizado para o seu negócio + Gestão e Engajamento Redes
+                  Sociais, curtidas, comentários e seguidores.
+                </p>
+              </div>
+              <div>
+                <p>R$ 1599,99</p>
+                <p>R$ 699,99</p>
+              </div>
+              <a href="">Clique e Saiba Mais</a>
+            </div>
+            <div>
+              <div>
+                <h1>Plano Avançado</h1>
+                <p>
+                  Todas as opções dos Planos Iniciantes e Intermediário + Opção
+                  de Site Com Páginas + Gestão de Conteúdo de Publicações e
+                  Stories Mensais + Automações Simples Instagram e Facebook +
+                  Scripts de Vendas
+                </p>
+              </div>
+              <div>
+                <p>R$ 1999,99</p>
+                <p>R$ 799,99</p>
+              </div>
+              <a href="">Clique e Saiba Mais</a>
+            </div>
+            <div>
+              <div>
+                <h1>Plano Advantage Pro</h1>
+                <p>
+                  Todas as Opções dos Planos Anteriores + Gestão de Marketing
+                  Digital Meta ads, Google Ads + Gestão Completa Whatsapp
+                  incluindo Automação de atendimento e venda, gerenciamento de
+                  catálogos, criação de numeros empresariais, gestão de
+                  multiplos atendimentos, métodos de pagamento e entrega.
+                </p>
+              </div>
+              <div>
+                <p>RS 4999,00</p>
+                <p>R$ 1499,99</p>
+              </div>
+              <a href="">Clique e Saiba Mais</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="contato" className="bg-[#DDD8D3]">
+        <div>
+          <img
+            src="/assets/img/7.png"
+            alt="Imagem de uma atendente de telemarketing"
+          />
+          <div>
+            <div>
+              <h1>
+                Profissionais sempre prontos para te <span>Atender</span>
+              </h1>
+              <div>
+                <img src="/assets/icones/5.svg" alt="Icone de um telefone" />
+                <img src="/assets/icones/6.svg" alt="Icone de um telefone" />
+                <img src="/assets/icones/7.svg" alt="Icone de um telefone" />
+              </div>
+            </div>
+            <div>
+              <form action="">
+                <div>
+                  <div>
+                    <label htmlFor="">Nome</label>
+                    <input type="text" />
+                  </div>
+                  <div>
+                    <label htmlFor="">Telefone</label>
+                    <input type="text" />
+                  </div>
+                  <div>
+                    <label htmlFor="">Segmento</label>
+                    <input type="text" />
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor="">Email</label>
+                    <input type="text" />
+                  </div>
+                  <div>
+                    <label htmlFor="">Empresa</label>
+                    <input type="text" />
+                  </div>
+                  <div>
+                    <label htmlFor="">Deixe a sua Mensagem</label>
+                    <textarea />
+                  </div>
+                </div>
+                <button type="submit">Enviar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="mapa">
+        <div>
+          <h1>
+            Onde Estamos <span>?</span>
+          </h1>
+          <img src="/assets/img/8.png" alt="Imagem de um mapa" />
+        </div>
+      </section>
+      <footer>
+        <div>
+          <div className="bg-[#474747]">
+            <img src="/assets/logotipo.png" alt="logotipo" />
+            <p>
+              Transformando Visões em Realidade: Seu Negócio no Ápice do
+              Sucesso.
+            </p>
+            <div>
+              <img src="/assets/icones/8.svg" alt="Icone do facebook" />
+              <img src="/assets/icones/10.svg" alt="Icone do instagram" />
+              <img src="/assets/icones/12.svg" alt="Icone do linkedin" />
+            </div>
+          </div>
+          <div className="bg-[#292929]">
+            <p>Todos os direitos reservados. | © 2020 ALTUORI MARKETING</p>
+            <p>
+              Uma empresa do <a href="">Grupo Altuori</a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </main>
+  );
+}
